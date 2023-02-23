df26be2684bac4675318a13cd32d847643231e8b
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-02-23 10:31:11 +0530

    Props completed

--- ReactJs/my-app/src/App.js
+++ ReactJs/my-app/src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -26,26 +26,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      ></ExpenseItem>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ ReactJs/my-app/src/components/Expenses/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+function Expenses({ expenses }) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+export default Expenses;
