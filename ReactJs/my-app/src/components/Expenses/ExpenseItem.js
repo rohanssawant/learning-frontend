7a758cad35f380eed634e262d748eac8301e5306
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-02-23 21:01:33 +0530

    started Topic: State

--- ReactJs/my-app/src/components/Expenses/ExpenseItem.js
+++ ReactJs/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem({ title, date, amount }) {
+  const [varTitle, setTitle] = useState(title);
+  const onClickHandler = () => {
+    setTitle("updated");
+    alert("Clicked");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{varTitle}</h2>
       </div>
       <div className="expense-item__price">${amount}</div>
+      <button onClick={onClickHandler}>Change Title</button>
     </Card>
   );
 }
