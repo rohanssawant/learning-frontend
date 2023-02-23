04ae9702703b31b3ca03f3c0cee3b04f93d3b7ec
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-02-23 10:31:11 +0530

    Props completed

--- ReactJs/my-app/src/components/Expenses/ExpenseItem.js
+++ ReactJs/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,15 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem({ title, date, amount }) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
-      <div className="expense-item__description">{title}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+      </div>
       <div className="expense-item__price">${amount}</div>
-    </div>
+    </Card>
   );
 }
 
