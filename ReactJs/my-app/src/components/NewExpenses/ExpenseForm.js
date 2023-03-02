c86adfb38f04f3375d385ec35a9eb23f1824efd4
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-03-02 11:12:43 +0530

    Multiple states & prev state use cases

--- ReactJs/my-app/src/components/NewExpenses/ExpenseForm.js
+++ ReactJs/my-app/src/components/NewExpenses/ExpenseForm.js
@@ -1,19 +1,69 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 import "./NewExpenses.css";
 function ExpenseForm() {
+  /** useState mutiple times */
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  /** This is how to manage in single state, not recommended */
+  const [userInputs, setUserInputs] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
+
+  const titleChangeEventHandler = (event) => {
+    console.log("titleChangeEventHandler: " + event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInputs((prevState) => {
+      return {
+        ...prevState,
+        title: event.target.value,
+      };
+    });
+  };
+
+  const amtChangeEventHandler = (event) => {
+    console.log("amtChangeEventHandler: " + event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInputs((prevState) => {
+      return {
+        ...prevState,
+        amount: event.target.value,
+      };
+    });
+  };
+
+  const dateChangeEventHandler = (event) => {
+    console.log("dateChangeEventHandler: " + event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInputs((prevState) => {
+      return {
+        ...prevState,
+        date: event.target.value,
+      };
+    });
+  };
   return (
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" />
+        <input type="text" onChange={titleChangeEventHandler} />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0.1" step="0.1" />
+        <input
+          type="number"
+          min="0.1"
+          step="0.1"
+          onChange={amtChangeEventHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" />
+        <input type="date" onChange={dateChangeEventHandler} />
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
