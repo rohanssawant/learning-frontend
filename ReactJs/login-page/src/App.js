5eb0b1d0c080dcad22a7c4735c11db47e716e092
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-03-08 23:08:54 +0530

    Created Login page starter components

---
+++ ReactJs/login-page/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
