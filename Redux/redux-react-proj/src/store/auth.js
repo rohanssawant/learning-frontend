e6fcc46673d037091b4ad22b4a02fc2a18ff27b7
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-06-07 12:14:45 +0530

    Splitting redux slices

---
+++ Redux/redux-react-proj/src/store/auth.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isAuthenticated: false },
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+// Actions. Redux toolkit automatically creates dispatchable actions for us w a unique identifier
+export const authActions = authSlice.actions;
+
+export default authSlice;
--- Redux/redux-react-proj/src/store/index.js
+++ Redux/redux-react-proj/src/store/index.js
@@ -1,40 +1,6 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { counter: 0, showCounter: true },
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: { isAuthenticated: false },
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-// Actions. Redux toolkit automatically creates dispatchable actions for us w a unique identifier
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter";
+import authSlice from "./auth";
 
 // CreateStore
 const store = configureStore({
