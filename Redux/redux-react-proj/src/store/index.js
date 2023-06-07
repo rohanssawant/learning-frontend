c968d4cad653c7314ada41238de53cdd59ddfab6
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-06-07 12:06:52 +0530

    Adding redux multiple state slices to proj

--- Redux/redux-react-proj/src/store/index.js
+++ Redux/redux-react-proj/src/store/index.js
@@ -19,9 +19,25 @@ const counterSlice = createSlice({
   },
 });
 
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isAuthenticated: false },
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 // Actions. Redux toolkit automatically creates dispatchable actions for us w a unique identifier
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 // CreateStore
-const store = configureStore({ reducer: counterSlice.reducer });
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
 export default store;
