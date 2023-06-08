a913708f4ffc2bb552526ca09016dfecf3a5a3b4
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-06-08 11:15:41 +0530

    Defining routes

--- react-routing/basic-p1/src/App.js
+++ react-routing/basic-p1/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  { path: "/home", element: <Home /> },
+  { path: "/product", element: <Product /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
