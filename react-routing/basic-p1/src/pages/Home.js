3fe32a3cdcfe6e41971dd92b0e3d3ac649e49aa5
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-06-12 11:36:29 +0530

    Navigating programatically from inside code using useNavigate

--- react-routing/basic-p1/src/pages/Home.js
+++ react-routing/basic-p1/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigateToProducts = useNavigate();
+  const navigationHandler = () => {
+    navigateToProducts("/product");
+  };
   return (
     <>
       <h1>This is my home page</h1>
+      <button onClick={navigationHandler}>Navigate to Product</button>
     </>
   );
 };
