453cb691b3c35e905579e355672309547a12d180
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-06-21 19:26:17 +0530

    useDefer() to load components when still waiting for data

--- react-routing/project-2/frontend/src/components/EventsList.js
+++ react-routing/project-2/frontend/src/components/EventsList.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import classes from "./EventsList.module.css";
 
 function EventsList({ events }) {
+  // console.log(events);
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
--- react-routing/project-2/frontend/src/pages/Events.js
+++ react-routing/project-2/frontend/src/pages/Events.js
@@ -1,7 +1,8 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     // setError("Fetching events failed.");
@@ -13,19 +14,27 @@ export async function loader() {
     // });
     throw json({ message: "Could not load events" }, { status: 500 });
   } else {
-    return response;
+    const respParsed = await response.json();
+    console.log(respParsed);
+    return respParsed.events;
   }
 }
 
+export function loader() {
+  return defer({ events: loadEvents() });
+}
+
 function EventsPage() {
-  const data = useLoaderData();
-  if (data.isError) return <p>{data.message}</p>;
-  const events = data.events;
+  const { events } = useLoaderData();
+  // if (data.isError) return <p>{data.message}</p>;
+  // const events = data.events;
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading....</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
