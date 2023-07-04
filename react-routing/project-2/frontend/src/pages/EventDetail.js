6d2dbc77e21f509ce662d340077c2ca017b29d56
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-07-04 11:10:52 +0530

    React routing module completed

--- react-routing/project-2/frontend/src/pages/EventDetail.js
+++ react-routing/project-2/frontend/src/pages/EventDetail.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
+  Await,
+  defer,
   json,
   redirect,
   useLoaderData,
@@ -7,18 +9,44 @@ import {
   useRouteLoaderData,
 } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
-export async function loader({ request, params }) {
-  const response = await fetch(
-    `http://localhost:8080/events/${params.eventId}`
-  );
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    // setError("Fetching events failed.");
+    console.log(response);
+    // return { isError: true, message: "Could not load events" };
+    // throw { message: "Could not load events" };
+    // throw new Response(JSON.stringify({ message: "Could not load events" }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Could not load events" }, { status: 500 });
+  } else {
+    const respParsed = await response.json();
+    console.log(respParsed);
+    return respParsed.events;
+  }
+}
+
+async function loadEvent(eventId) {
+  const response = await fetch(`http://localhost:8080/events/${eventId}`);
   if (!response.ok) {
     throw json({ message: "Could not fetch selected event!" }, { status: 500 });
   } else {
-    return response;
+    const respParsed = await response.json();
+    return respParsed.event;
   }
 }
-// Delete an event
+
+export async function loader({ request, params }) {
+  return defer({
+    events: loadEvents(), // This will come or not does not depend if page is rendered or not
+    event: await loadEvent(params.eventId), // This await will make sure that details are loaded before page is rendered
+  });
+}
+
+// Action to Delete an event using submit()
 export async function action({ request, params }) {
   const resp = await fetch(`http://localhost:8080/events/${params.eventId}`, {
     method: request.method,
@@ -33,13 +61,22 @@ export async function action({ request, params }) {
 }
 
 function EventDetailPage() {
-  const params = useParams();
   // const response = useLoaderData();
-  const response = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
-      <EventItem event={response.event} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 }
