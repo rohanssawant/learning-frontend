90b36faeeef93403e385a38b15b2f1e252740565
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-07-06 10:42:43 +0530

    Added Form validations and handling

--- react-auth/frontend/src/components/AuthForm.js
+++ react-auth/frontend/src/components/AuthForm.js
@@ -1,15 +1,31 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
+import ErrorPage from "../pages/Error";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  const data = useActionData();
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
-
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -22,7 +38,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting.." : "Save"}
+          </button>
         </div>
       </Form>
     </>
--- react-auth/frontend/src/pages/Authentication.js
+++ react-auth/frontend/src/pages/Authentication.js
@@ -1,4 +1,31 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
+export async function action({ request }) {
+  // Check if mode is login or create
+  console.log(`in here!!`);
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode");
+  console.log(mode);
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  console.log(authData);
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: request.method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(authData),
+  });
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { status: 500 });
+  }
+  return redirect("/");
+}
 
 function AuthenticationPage() {
   return <AuthForm />;
